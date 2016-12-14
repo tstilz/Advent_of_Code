021b55c4fffc4726fada6aaa25f1d4e80673cd5d
Stilwell, Thomas <Thomas.Stilwell@nordicsemi.no>
2016-12-14 14:02:29 +0100

    autopep8

--- 2_code.py
+++ 2_code.py
@@ -7,11 +7,13 @@ INPUT = ['RDLULDLDDRLLLRLRULDRLDDRRRRURLRLDLULDLDLDRULDDLLDRDRUDLLDDRDULLLULLDUL
          'LRUULRRUDUDDLRRDURRUURDURURLULRDUUDUDLDRRULURUDURURDRLDDLRUURLLRDLURRULRRRUDULRRULDLUULDULLULLDUDLLUUULDLRDRRLUURURLLUUUDDLLURDUDURULRDLDUULDDRULLUUUURDDRUURDDDRUUUDRUULDLLULDLURLRRLRULRLDLDURLRLDLRRRUURLUUDULLLRRURRRLRULLRLUUDULDULRDDRDRRURDDRRLULRDURDDDDDLLRRDLLUUURUULUDLLDDULDUDUUDDRURDDURDDRLURUDRDRRULLLURLUULRLUDUDDUUULDRRRRDLRLDLLDRRDUDUUURLRURDDDRURRUDRUURUUDLRDDDLUDLRUURULRRLDDULRULDRLRLLDRLURRUUDRRRLRDDRLDDLLURLLUDL',
          'ULURLRDLRUDLLDUDDRUUULULUDDDDDRRDRULUDRRUDLRRRLUDLRUULRDDRRLRUDLUDULRULLUURLLRLLLLDRDUURDUUULLRULUUUDRDRDRUULURDULDLRRULUURURDULULDRRURDLRUDLULULULUDLLUURULDLLLRDUDDRRLULUDDRLLLRURDDLDLRLLLRDLDRRUUULRLRDDDDRUDRUULDDRRULLDRRLDDRRUDRLLDUDRRUDDRDLRUDDRDDDRLLRDUULRDRLDUDRLDDLLDDDUUDDRULLDLLDRDRRUDDUUURLLUURDLULUDRUUUDURURLRRDULLDRDDRLRDULRDRURRUDLDDRRRLUDRLRRRRLLDDLLRLDUDUDDRRRUULDRURDLLDLUULDLDLDUUDDULUDUDRRDRLDRDURDUULDURDRRDRRLLRLDLU']
 
+
 class RUNNER():
+
     def __init__(self):
-      self.answer = None
-      self.input = None
-      self.part = None
+        self.answer = None
+        self.input = None
+        self.part = None
 
     def setup(self, input=None, answer=None, part=0):
         self.input = input
@@ -19,12 +21,14 @@ class RUNNER():
         self.result = self.solve(input)
         if (answer is not None):
             if (self.result == answer):
-                print ('UnitTest Passed!')
+                print('UnitTest Passed!')
             else:
-                print ('UnitTest FAILED!   Correct answer is %s, Your result is %s.' % (answer, self.result))
+                print(
+                    'UnitTest FAILED!   Correct answer is %s, Your result is %s.' %
+                    (answer, self.result))
 
         else:
-           print ('                      Answer = %s' % self.result)
+            print('                      Answer = %s' % self.result)
 
     def update_finger_pos(self, cmd):
         prev_x = self.finger_pos['x']
@@ -32,28 +36,27 @@ class RUNNER():
 
         # Move Finger
         if cmd == 'U':
-            self.finger_pos['y'] -=1
+            self.finger_pos['y'] -= 1
         if cmd == 'D':
-            self.finger_pos['y'] +=1
+            self.finger_pos['y'] += 1
 
         if cmd == 'R':
-            self.finger_pos['x'] +=1
+            self.finger_pos['x'] += 1
         if cmd == 'L':
-            self.finger_pos['x'] -=1
+            self.finger_pos['x'] -= 1
 
         # Boundry Checks
         try:
-            self.keypad[(self.finger_pos['x'],self.finger_pos['y'])]
+            self.keypad[(self.finger_pos['x'], self.finger_pos['y'])]
         except KeyError:
             self.finger_pos['x'] = prev_x
             self.finger_pos['y'] = prev_y
 
     def set_start_button(self):
         if self.part is 1:
-            self.finger_pos = {'x':1, 'y':1}
+            self.finger_pos = {'x': 1, 'y': 1}
         elif self.part is 2:
-            self.finger_pos = {'x':0, 'y':2}
-
+            self.finger_pos = {'x': 0, 'y': 2}
 
     def generate_keypad(self):
         if self.part is 1:
@@ -61,7 +64,7 @@ class RUNNER():
             key_num = 1
             for y in range(3):
                 for x in range(3):
-                    self.keypad.update({(x,y):hex(key_num)[2:].upper()})
+                    self.keypad.update({(x, y): hex(key_num)[2:].upper()})
                     key_num += 1
                     # print ('finger=(%d,%d), button=%s' %(x,y,self.keypad[(x,y)]))
 
@@ -70,17 +73,16 @@ class RUNNER():
             key_num = 1
             for y in range(5):
                 for x in range(5):
-                    if (((x+y) <= 1) or ((x+y) >= 7)):
+                    if (((x + y) <= 1) or ((x + y) >= 7)):
                         continue
-                    if (((x-y) >= 3) or ((x-y) <= -3)):
+                    if (((x - y) >= 3) or ((x - y) <= -3)):
                         continue
-                    self.keypad.update({(x,y):hex(key_num)[2:].upper()})
+                    self.keypad.update({(x, y): hex(key_num)[2:].upper()})
                     key_num += 1
                     # print ('finger=(%d,%d), button=%s' %(x,y,self.keypad[(x,y)]))
 
-
     def find_button_name(self):
-        return [self.keypad[(self.finger_pos['x'],self.finger_pos['y'])]]
+        return [self.keypad[(self.finger_pos['x'], self.finger_pos['y'])]]
 
     def push_keypad(self):
         # print ('Finger Position = %s' % self.finger_pos)
@@ -91,22 +93,36 @@ class RUNNER():
         self.generate_keypad()
         self.passcode = []
         for num in input:
-           for move in num:
-               self.update_finger_pos(move)
-           self.push_keypad()
+            for move in num:
+                self.update_finger_pos(move)
+            self.push_keypad()
         self.result = "".join([str(x) for x in self.passcode])
         # print ('The Bathroom Passcode is %s' % self.result)
         return self.result
 
 
 def main():
-  print ('Find the Bathroom Passcode : \n%s' % str(INPUT)[:80])
-  runner = RUNNER()
-  runner.setup(input=['ULL','RRDDD','LURDL','UUUUD'], answer='1985', part=1)
-  runner.setup(input=INPUT, answer='33444', part=1)
-  runner.setup(input=['ULL','RRDDD','LURDL','UUUUD'], answer='5DB3', part=2)
-  runner.setup(input=INPUT, answer=None, part=2)
-  return
+    print('Find the Bathroom Passcode : \n%s' % str(INPUT)[:80])
+    runner = RUNNER()
+    runner.setup(
+        input=[
+            'ULL',
+            'RRDDD',
+            'LURDL',
+            'UUUUD'],
+        answer='1985',
+        part=1)
+    runner.setup(input=INPUT, answer='33444', part=1)
+    runner.setup(
+        input=[
+            'ULL',
+            'RRDDD',
+            'LURDL',
+            'UUUUD'],
+        answer='5DB3',
+        part=2)
+    runner.setup(input=INPUT, answer=None, part=2)
+    return
 
 if __name__ == '__main__':
-   main()
+    main()
--- 3_code.py
+++ 3_code.py
@@ -1,22 +1,24 @@
 import os
 import sys
 
+
 class RUNNER():
+
     def __init__(self, problem_statement=''):
-      self.answer = None
-      self.input = None
-      self.part = None
-      self.problem_statement = problem_statement
+        self.answer = None
+        self.input = None
+        self.part = None
+        self.problem_statement = problem_statement
 
     def read_input(self, filename):
         self.input = []
         int_list = []
-        with open( filename) as f:
+        with open(filename) as f:
             lines = f.readlines()
             for line in lines:
-              line = [line[:6], line[6:10], line[10:15]]
-              line = [int(x) for x in line]
-              int_list += line
+                line = [line[:6], line[6:10], line[10:15]]
+                line = [int(x) for x in line]
+                int_list += line
 
         if self.part is 1:
             while (len(int_list) > 0):
@@ -40,17 +42,19 @@ class RUNNER():
     def setup(self, input=None, answer=None, part=0):
         self.part = part
         self.read_input(input)
-        print ('%s : \n%s' % (self.problem_statement, str(self.input)[:80]))
+        print('%s : \n%s' % (self.problem_statement, str(self.input)[:80]))
 
         self.result = self.solve(self.input)
         if (answer is not None):
             if (self.result == answer):
-                print ('UnitTest Passed!')
+                print('UnitTest Passed!')
             else:
-                print ('UnitTest FAILED!   Correct answer is %s, Your result is %s.' % (answer, self.result))
+                print(
+                    'UnitTest FAILED!   Correct answer is %s, Your result is %s.' %
+                    (answer, self.result))
 
         else:
-           print ('                      Answer = %s' % self.result)
+            print('                      Answer = %s' % self.result)
 
     def solve(self, input=None):
         self.result = 0
@@ -58,20 +62,24 @@ class RUNNER():
             triangle = sorted(triangle)
             valid = True
             for i in [0]:
-              if ((triangle[i%3]+triangle[(i+1)%3]) <= triangle[(i+2)%3]):
-                  valid = False
+                if ((triangle[i %
+                              3] + triangle[(i + 1) %
+                                            3]) <= triangle[(i + 2) %
+                                                            3]):
+                    valid = False
             if valid:
-              self.result += 1
+                self.result += 1
         return str(self.result)
 
-def main():
-  runner = RUNNER(problem_statement='Find valid trianges')
-  runner.setup(part=1, answer='1',   input='day_3_input_unittest.txt')
-  runner.setup(part=1, answer='983', input='day_3_input.txt')
-  runner.setup(part=2, answer='3',   input='day_3_input_unittest.txt')
-  runner.setup(part=2, answer=None,  input='day_3_input.txt')
 
-  return
+def main():
+    runner = RUNNER(problem_statement='Find valid trianges')
+    runner.setup(part=1, answer='1', input='day_3_input_unittest.txt')
+    runner.setup(part=1, answer='983', input='day_3_input.txt')
+    runner.setup(part=2, answer='3', input='day_3_input_unittest.txt')
+    runner.setup(part=2, answer=None, input='day_3_input.txt')
+
+    return
 
 if __name__ == '__main__':
-   main()
+    main()
